c14ea868a4ecb1087cba98c4ff0061b792d1f71d
Marcelo Escobar <escobar@br.systems>
2019-05-22 17:21:21 -0300

    Restaurant COntroller finished

---
+++ config/webpack/environment.js
@@ -0,0 +1,13 @@
+import { environment } from '@rails/webpacker';
+
+// Bootstrap 4 has a dependency over jQuery & Popper.js:
+import { ProvidePlugin } from 'webpack';
+environment.plugins.prepend('Provide',
+  new ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
+export default environment
